3d64909d52c3c3b77030ae245b986bcf324ab47a
agtayler95 <agtayler95@gmail.com>
2020-10-10 22:24:01 -0600

    This is the Database DAO from the familyMap Server.
    Also, I'm hoping that the database file I put together in SQLite comes through the "push". Feel free to use it or not.

--- src/main/java/DAOs/Database.java
+++ src/main/java/DAOs/Database.java
@@ -7,5 +7,63 @@ import java.sql.Statement;
 
 public class Database {
     private Connection conn;
-    public Connection openConnection() throws DataAccessException {}
+    public Connection openConnection() throws DataAccessException {
+        try {
+            //The path assumes you start in the root of your project unless given a non-relative path
+            final String CONNECTION_URL = "jdbc:sqlite:White_Chapel_DB.db";
+
+            // Open a database connection to the file given in the path
+            conn = DriverManager.getConnection(CONNECTION_URL);
+
+            // Start a transaction
+            conn.setAutoCommit(false);
+        } catch (SQLException e) {
+            e.printStackTrace();
+            throw new DataAccessException("Unable to open connection to database");
+        }
+
+        return conn;
+    }
+
+    public Connection getConnection() throws DataAccessException {
+        if(conn == null) {
+            return openConnection();
+        } else {
+            return conn;
+        }
+    }
+
+    public void closeConnection(boolean commit) throws DataAccessException {
+        try {
+            if (commit) {
+                //This will commit the changes to the database
+                conn.commit();
+            } else {
+                //If we find out something went wrong, pass a false into closeConnection and this
+                //will rollback any changes we made during this connection
+                conn.rollback();
+            }
+
+            conn.close();
+            conn = null;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            throw new DataAccessException("Unable to close database connection");
+        }
+    }
+
+    public void clearTables() throws DataAccessException
+    {
+
+        try (Statement stmt = conn.createStatement()){
+            String sql = "DELETE FROM CONNECTIONS";
+            stmt.executeUpdate(sql);
+            stmt.executeUpdate("DELETE FROM JACK_MOVES");
+            stmt.executeUpdate("DELETE FROM INVESTIGATIONS");
+            stmt.executeUpdate("DELETE FROM POSSIBLE_LOCATIONS");
+        } catch (SQLException e) {
+            throw new DataAccessException("SQL Error encountered while clearing tables");
+        }
+    }
 }
+
