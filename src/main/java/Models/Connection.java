82bd545324553a5a39e52314fb4ddb20ef138f7d
joanna-hugo <joanna.hugo.42@gmail.com>
2020-10-21 13:04:18 -0600

    complete Connection model

--- src/main/java/Models/Connection.java
+++ src/main/java/Models/Connection.java
@@ -1,4 +1,34 @@
 package Models;
 
 public class Connection {
+  int from_node;
+  int to_node;
+  ConnectionType connectionType;
+
+  public Connection(int from_noode, int to_node, String connType) throws Exception {
+	this.from_node = from_noode;
+	this.to_node = to_node;
+	if(connType.toLowerCase() == ConnectionType.ALLEY.getType()){
+	  connectionType= ConnectionType.ALLEY;
+	}else if(connType.toLowerCase() == ConnectionType.STREET.getType()){
+	  connectionType = ConnectionType.STREET;
+	}else{
+	  throw new Exception("invalid connection type provided: " + connType);
+	}
+  }
+}
+
+enum ConnectionType{
+  ALLEY("alley"),
+  STREET("street");
+
+  private String connection_type;
+
+  ConnectionType(String connection_type) {
+	this.connection_type = connection_type;
+  }
+
+  public String getType() {
+	return connection_type;
+  }
 }
