8d4b2527251ee4fd6fcd93f83b49d1eedbd21898
joanna-hugo <joanna.hugo.42@gmail.com>
2020-10-28 11:10:17 -0600

    working JackMoves functions

--- src/main/java/Services/JackMoveService.java
+++ src/main/java/Services/JackMoveService.java
@@ -8,13 +8,24 @@ public class JackMoveService {
 
   PossibleLocationDAO locations_dao =  new PossibleLocationDAO();
   JackMoveDAO jack_move_dao = new JackMoveDAO();
+  ConnectionService connectionService = new ConnectionService();
 
   public void jackMovesRandom(){
     //TODO
   }
 
-  public void jackMoves(int destination){
-    //TODO
+  public void jackMoves(int destination, String move_type) throws Exception{
+    PossibleLocationService possibleLocationService = new PossibleLocationService();
+    int current_node = jack_move_dao.getCurrentJackLocation();
+    int current_turn = jack_move_dao.getTurn();
+    current_turn ++;
+
+    if(connectionService.isValidMove(current_node, destination, move_type)){
+      jack_move_dao.insertJackMove(current_turn, destination, move_type);
+    }else{
+      throw new Exception("Invalid Move");
+    }
+    possibleLocationService.jackMovedUpdateTable(current_turn, move_type);
   }
 
   public int getNumAlleysUsed(){
