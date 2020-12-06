0a58c3e0555165555558f7526c92d4d818330ef0
joanna-hugo <joanna.hugo.42@gmail.com>
2020-12-06 00:00:57 -0700

    Location model reflects graph database

--- src/main/java/Models_Graph/Location_Node.java
+++ src/main/java/Models_Graph/Location_Node.java
@@ -1,25 +1,24 @@
 package Models_Graph;
 
 
-import org.neo4j.driver.types.Node;
-
 public class Location_Node {
     private final int Number;
-    private int Turn_Investigated;
-    private boolean Clue_Found;
-    private boolean Jack_Is_Here;
+    private int Turn_investigated;
+    private boolean Clue_found;
+    private boolean Jack_Is_Here; //Don't have yet but can add as we go?
+    private boolean Jack_visited;
 
     public Location_Node(int number) {
         Number = number;
-        Turn_Investigated = 0;
-        Clue_Found = false;
+        Turn_investigated = 0;
+        Clue_found = false;
         Jack_Is_Here = false;
     }
 
     public Location_Node(int number, int TI, boolean CF, boolean JIH) {
         Number = number;
-        Turn_Investigated = TI;
-        Clue_Found = CF;
+        Turn_investigated = TI;
+        Clue_found = CF;
         Jack_Is_Here = JIH;
     }
 
@@ -27,20 +26,20 @@ public class Location_Node {
         return Number;
     }
 
-    public int getTurn_Investigated() {
-        return Turn_Investigated;
+    public int getTurn_investigated() {
+        return Turn_investigated;
     }
 
-    public void setTurn_Investigated(int turn_Investigated) {
-        Turn_Investigated = turn_Investigated;
+    public void setTurn_investigated(int turn_investigated) {
+        Turn_investigated = turn_investigated;
     }
 
-    public boolean isClue_Found() {
-        return Clue_Found;
+    public boolean isClue_found() {
+        return Clue_found;
     }
 
-    public void setClue_Found(boolean clue_Found) {
-        Clue_Found = clue_Found;
+    public void setClue_found(boolean clue_found) {
+        Clue_found = clue_found;
     }
 
     public boolean isJack_Is_Here() {
@@ -55,8 +54,8 @@ public class Location_Node {
     public String toString() {
         return "Location_Node{" +
                 "Number=" + Number +
-                ", Turn_Investigated=" + Turn_Investigated +
-                ", Clue_Found=" + Clue_Found +
+                ", Turn_Investigated=" + Turn_investigated +
+                ", Clue_Found=" + Clue_found +
                 ", Jack_Is_Here=" + Jack_Is_Here +
                 '}';
     }
