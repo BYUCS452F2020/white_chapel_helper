978c2c1fd4e1a89285ba15e040a4b55316a3c777
ngillis13 <64992967+ngillis13@users.noreply.github.com>
2020-12-05 22:31:43 -0800

    Update LocationDAOTest.java

--- src/test/java/DAOs_Graph/LocationDAOTest.java
+++ src/test/java/DAOs_Graph/LocationDAOTest.java
@@ -1,13 +1,40 @@
 package DAOs_Graph;
 
 import Models.Connection;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import DAOs_Graph.LocationDAO;
 import DAOs_Graph.Database_Graph;
 
 
 public class LocationDAOTest {
 
-}
+    private LocationDAO dao = new LocationDAO();
+
+    @AfterEach
+    public void cleanUp(){
+        dao.deleteLocations();
+    }
+
+    @Test
+    void addConnections(){
+
+    }
+
+    @Test
+    void addNodes(){
+        dao.addNode(1);
+        dao.addNode(2);
+        dao.addNode(3);
+        dao.addNode(5);
+
+        dao.addStreetConnection(1, 3);
+        dao.addStreetConnection(1, 2);
+        dao.addStreetConnection(2, 5);
+
+        System.out.println(dao.getAllConnections(1).elementAt(0));
+    }
+
+    @Test
+    void getConnections(){
+
+    }
